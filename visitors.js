8c0dae3205268d5e7bb1b2fdba6a6bd02fbbbb1c
mullinsacc <mullinsacc@gmail.com>
2021-07-09 10:50:29 -0400

    Create visitors.js

---
+++ visitors.js
@@ -0,0 +1,73 @@
+
+
+
+var user_id = 'IkoraRey';
+var user_email = 'alisyn.collins+3@gmail.com';
+var user_full_name = 'Ikora Rey';
+var user_account_id = 'Operation_Babydog';
+var user_language = 'en_GB';
+var user_random_data = '';
+var user_boolean = true;
+
+/*
+var user_id = 'AnaBray';
+var user_email = 'alisyn.collins+2@gmail.com';
+var user_full_name = 'Ana Bray';
+var user_account_id = 'Clovis_Bray_Corporation';
+var user_language = 'en_GB';
+var user_random_data = 'ThisIsNewData';
+var user_boolean = 0;
+*/
+//var user_date1 = '2020-03-27T00:00:00.000000Z';
+//var user_date2 = '2018-03-27T00:08:21.940412Z'
+
+
+/*
+var user_id = 'ElsieBrayXO';
+var user_email = 'alisyn.collins+4@gmail.com';
+var user_full_name = 'Elsie Bray';
+var user_account_id = 'Clovis_Bray_Corporation';
+var user_language = 'en_GB'
+*/
+
+/*
+var user_id = 'ElsieBrayXO';
+var user_email = 'alisyn.collins+4@gmail.com';
+var user_full_name = 'Elsie Bray';
+var user_account_id = 'Stasis, LLC';
+var user_language = 'en_GB';
+var user_random_data = 'The Stranger'
+*/
+
+/*
+var user_id = 'MaraSov';
+var user_email = 'alisyn.collins+1@gmail.com';
+var user_full_name = 'Queen Mara Sov';
+var user_account_id = 'Reef_Awoken';
+var user_language = 'en_GB';
+var user_random_data = 'Queen'
+*/
+
+/*
+var user_id = 'ErisMorn';
+var user_email = 'alisyn.collins@gmail.com';
+var user_full_name = 'Ol Bleedy Eyes';
+var user_account_id = 'Hashladûn_Industries';
+var user_random_data = 'Warlock'
+*/
+
+/*
+var user_id = '1234567890-1234567890-1234567890-1234567890-InsanelyLongUserID';
+var user_email = 'alisyn.collins+1@gmail.com';
+var user_full_name = 'InsanelyLongUserID';
+var user_account_id = 'Reef_Awoken';
+var user_random_data = 'not even destiny related'
+*/
+
+/*
+var user_id = 'AccountlessVisitor';
+var user_email = 'alisyn.collins+4@gmail.com';
+var user_full_name = 'The Drifter';
+var user_language = 'en_GB';
+var user_random_data = 'Gambito'
+*/
